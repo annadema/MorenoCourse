dd6cc61609d7fdf1493e601d9a8dde117f95e43a
Annamaria De Martino <annamaria.demartino@gmail.com>
2024-06-10 21:04:26 +0200

    Aggiornata l'applicazione ad ASP.NET Core 2.2

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using MorenoCourse.Controllers;
 
@@ -16,7 +17,8 @@ namespace MorenoCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
